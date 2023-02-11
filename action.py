246ea03331040ee759201c8877446bdf025bc59a
D.C. Compton <code@dccompton.com>
2023-02-10 19:31:25 -0600

    A bit of cleanup.

--- action.py
+++ action.py
@@ -1,7 +1,10 @@
 from github import Github
 from dotenv import load_dotenv
+from graphviz import Digraph,ENGINES
+
 import os, sys, pypandoc
 import base64
+
 
 class Action(object):
     ghConn = None
@@ -57,3 +60,50 @@ class Action(object):
 
     def b64(self, string):
         return str(base64.b64encode(string.encode('ascii')))
+
+    def make_diagram(self, nodes_and_edges, title='Workflow Diagram', colors={'default':'#cccccc'}, filename_prefix='Workflow'):
+        g = Digraph(format='svg')
+        g.attr(scale='2', label=title, fontsize='12')
+
+        g.attr('node', shape='box', style='filled')
+
+        all_sub_ed = []
+        for node in nodes_and_edges['nodes']:
+            fillcolor=colors['default']
+            if node['type'] in colors:
+                fillcolor = colors[node['type']]
+
+            if 'belongs_to' in node:
+                #** GROUP THE WORKFLOW ITEMS   
+                with g.subgraph(name=f"cluster_{node['belongs_to']}") as job_group:
+
+                    sub_ed = []
+                    for edge in nodes_and_edges['edges']:
+                        if 'belongs_to' in edge and edge['belongs_to'] == node['belongs_to']:
+                            entry = (edge['source'], edge['target'])
+                            if entry not in all_sub_ed:
+                                sub_ed.append(entry)
+                                all_sub_ed.append(entry)
+                    job_group.attr(label="Workflow")
+                    
+                    job_group.node(node['id'], label=f"<{self.make_table(node)}>", fillcolor=fillcolor)
+                    job_group.edges(sub_ed)
+                
+            else:
+                g.node(node['id'], label=f"<{self.make_table(node)}>", fillcolor=fillcolor)
+
+        ed=[]
+        for edge in nodes_and_edges['edges']:
+            if 'belongs_to' not in edge:
+                entry = (edge['source'], edge['target'])
+                if entry not in ed:
+                    ed.append((edge['source'], edge['target']))
+
+
+        g.edges(ed)
+
+        # dot or fdp
+        g.render(filename=filename_prefix, engine="dot")
+        g = None
+
+        return f"\n\n---\n\n![Graphical Representation of {title}]({filename_prefix}.svg)"
